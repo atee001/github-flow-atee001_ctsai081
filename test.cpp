6726f054731834773cefd55058e56cfed2386396
atee001 <atee001@ucr.edu>
2021-04-10 12:08:12 -0700

    Fixed main

--- test.cpp
+++ test.cpp
@@ -1,42 +1,34 @@
 #include "c-echo.h"
+#include "c-count.h"
 
 #include "gtest/gtest.h"
 
 TEST(EchoTest, HelloWorld) {
-    char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "hello"; test_val[2] = "world";
+    char* test_val[3]; test_val[0] = "./c-echo-count"; test_val[1] = "hello"; test_val[2] = "world";
     EXPECT_EQ("hello world", echo(3,test_val));
-//	char* test_v1[3]; test_v1[0] = "./c-echo"; test_v1[1] = "Andrew"; test_v1[2] = "Tee";
-//        EXPECT_EQ("Andrew Tee", echo(3,test_v1));
-//	char* test_v2[2]; test_v2[0] = "./c-echo"; test_v2[1] = "Linux";
-//	EXPECT_EQ("Linux", echo(2,test_v2));
-//	char* test_v3[5]; test_v3[0] = "./c-echo"; test_v3[1] = "Last\n"; test_v3[2] = "Test\n"; test_v3[3] = "Harness\n"; test_v3[4] = "!";
-//	EXPECT_EQ("Last\n Test\n Harness\n !", echo(5,test_v3)); 
-
 }
 
-TEST(EchoTest, AndrewTee){
- char* test_v1[3]; test_v1[0] = "./c-echo"; test_v1[1] = "Andrew"; test_v1[2] = "Tee";
-        EXPECT_EQ("Andrew Tee", echo(3,test_v1));
-
+TEST(EchoTest, EmptyString) {
+    char* test_val[1]; test_val[0] = "./c-echo-count";
+    EXPECT_EQ("", echo(1, test_val));
 }
 
-TEST(EchoTest, Linux){
- char* test_v2[2]; test_v2[0] = "./c-echo"; test_v2[1] = "Linux";
-        EXPECT_EQ("Linux", echo(2,test_v2));
+TEST(CountTest, HelloWorld) {
+    std::string test_str = "hello world";
+    EXPECT_EQ(2, count(test_str));
 }
 
-TEST(EchoTest, LastTestHarness){
- char* test_v3[5]; test_v3[0] = "./c-echo"; test_v3[1] = "Last\n"; test_v3[2] = "Test\n"; test_v3[3] = "Harness\n"; test_v3[4] = "!";
-        EXPECT_EQ("Last\n Test\n Harness\n !", echo(5,test_v3));
-
+TEST(CountTest, EmptyString) {
+    std::string test_str = "";
+    EXPECT_EQ(0, count(test_str));
 }
 
-TEST(EchoTest, EdgeCase){
-char* test_v4[2]; test_v4[0] = "./c-echo"; test_v4[1] = " "; 
-	EXPECT_EQ(" ", echo(2,test_v4));
+TEST(CountTest, ManySpaces) {
+    std::string test_str = "   this   string has     weird   spacing";
+    EXPECT_EQ(5, count(test_str));
 }
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
